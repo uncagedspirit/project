e1103da8a40d59e6e74b8c7096459bff88318026
Saakshi Kobarne <saakshik15@gmail.com>
2025-03-21 10:19:33 +0000

    Updated main.cpp by adding displayMenu()

--- Application/src/main.cpp
+++ Application/src/main.cpp
@@ -2,14 +2,27 @@
 using namespace std;
 
 //display file
-//functional file
+//functional logic file
 
-// int main(){
 
-//     do(){
-        
-//     }
-//     while()
+int displayMenu(){
+    cout<< "Plot shape using gnuplot.\n";
+    cout<<"\nWhat shape do you want to plot?\n";
+    cout<<" 0. I want to exit \n 1. Point \n 2. Line \n 3. 3D Line \n 4. Square \n 5. Rectangle \n 6. Cube \n 7. Cuboid \n ";
+    
+    int choice; 
+    cout<< "Enter your choice : ";
+    cin>> choice;
+    
+    return choice;
+}
 
-//     return 0;
-// }
+int main(){
+
+    bool cont = true;
+
+    do{
+        int choice = displayMenu();
+        if(choice == 0 ) cont= false;
+    }while(cont);
+}
